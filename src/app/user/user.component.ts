a7a3f5be01c6bc2ff542f874781676db64f90c89
OmNai442003 <omnayinor99@gmail.com>
2024-11-07 23:03:27 -0800

    Signals usage

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 // import { Component, computed, signal } from '@angular/core';
-import { Component, Input } from '@angular/core';
+import { Component, computed, input, Input } from '@angular/core';
 // import { DUMMY_USERS } from '../dummy-users';
 
 // const randIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -32,10 +32,20 @@ export class UserComponent {
 
   // Now the new thing above was just for the look up
   // We mandate that we will surly pass this configurations and if they are not passed then error will be thrown
-  @Input({required:true}) avatar!: string;
-  @Input({required:true}) name!: string;
+  // @Input({required:true}) avatar!: string;
+  // @Input({required:true}) name!: string;
 
-  get imagePath(){
-    return 'assets/images/users/' + this.avatar;
-  }
+  // By use of signal
+  // Now from outside we send by means of signal or not
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  // get imagePath(){
+  //   return 'assets/images/users/' + this.avatar;
+  // }
+
+  // For the purpose of signal
+  imagePath = computed(()=>{
+    return 'assets/images/users/' + this.avatar();
+  })
 }
